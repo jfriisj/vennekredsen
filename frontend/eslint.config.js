c8096fd95500bee39e00ab76d99df8d5ff702335
jfriisj <jonfriis@live.com>
2026-09-16 12:55:42 +0200

    Modernize frontend tooling dependencies

---
+++ frontend/eslint.config.js
@@ -0,0 +1,29 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ["html/**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "script",
+      globals: {
+        ...globals.browser,
+        ...globals.jquery,
+        $: "readonly",
+        jQuery: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+      indent: ["error", 4],
+      "no-trailing-spaces": "error",
+      "eol-last": "error",
+      "no-multiple-empty-lines": ["error", { max: 2 }],
+    },
+  },
+];
